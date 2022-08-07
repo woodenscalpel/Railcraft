c76e5b58449a08075bb5b97f92790b977b9ce013
Yang Xizhi <60341015+GlodBlock@users.noreply.github.com>
2022-08-07 19:15:57 +0800

    fix drain (#26)
    i am curious about why nobody notice that is broken

--- src/main/java/mods/railcraft/common/blocks/machine/beta/TileTankIronValve.java
+++ src/main/java/mods/railcraft/common/blocks/machine/beta/TileTankIronValve.java
@@ -1,6 +1,6 @@
-/* 
+/*
  * Copyright (c) CovertJaguar, 2014 http://railcraft.info
- * 
+ *
  * This code is the property of CovertJaguar
  * and may only be used with explicit written
  * permission unless otherwise specified on the
@@ -181,11 +181,10 @@ public class TileTankIronValve extends TileTankBase implements IFluidHandler, IC
 
     @Override
     public FluidStack drain(ForgeDirection from, int maxDrain, boolean doDrain) {
-        if (getPatternPositionY() - getPattern().getMasterOffsetY() != 1)
+        if (getPatternPositionY() - getPattern().getMasterOffsetY() > 1)
             return null;
         TankManager tMan = getTankManager();
         if (tMan != null)
-            //            maxDrain = Math.min(maxDrain, FLOW_RATE);
             return tMan.drain(0, maxDrain, doDrain);
         return null;
     }
